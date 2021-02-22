a53f5fe7bb9b3762c832c65cfcb4bfe8ab20d1fd
Fabien <flachaud@svp.com>
2021-02-22 19:33:03 +0100

    Refactoring Finder

--- src/Algorithm/Finder.php
+++ src/Algorithm/Finder.php
@@ -13,49 +13,46 @@ final class Finder
     }
 
     public function find(string $type): Comparator {
-        /** @var Comparator[] $tr */
-        $tr = [];
+        $comparator = new Comparator();
 
-        for ($i = 0, $iMax = count($this->people); $i < $iMax; $i++) {
-            for ($j = $i + 1, $jMax = count($this->people); $j < $jMax; $j++) {
-                $r = new Comparator();
+        if (count($this->people) < 2) {
+            return $comparator;
+        }
 
-                if ($this->people[$i]->birthDate < $this->people[$j]->birthDate) {
-                    $r->firstPerson = $this->people[$i];
-                    $r->secondPerson = $this->people[$j];
-                } else {
-                    $r->firstPerson = $this->people[$j];
-                    $r->secondPerson = $this->people[$i];
+        uasort($this->people, static function(Person $a, Person $b) {
+            return $a->birthDate->getTimestamp() > $b->birthDate->getTimestamp();
+        });
+
+        $comparator->firstPerson = current($this->people);
+
+        switch ($type) {
+            case FinderType::FURTHEST:
+                $comparator->secondPerson = end($this->people);
+                $comparator->delta = self::calculateDelta($comparator->firstPerson, $comparator->secondPerson);
+                break;
+
+            case FinderType::CLOSTEST:
+                $comparator->secondPerson = next($this->people);
+                $comparator->delta = self::calculateDelta($comparator->firstPerson, $comparator->secondPerson);
+
+                while (false !== $person = next($this->people)) {
+                    if (($delta = self::calculateDelta($comparator->secondPerson, $person)) < $comparator->delta) {
+                        $comparator->firstPerson  = $comparator->secondPerson;
+                        $comparator->secondPerson = $person;
+                        $comparator->delta = $delta;
+                    }
                 }
+                break;
 
-                $r->delta = $r->secondPerson->birthDate->getTimestamp() - $r->firstPerson->birthDate->getTimestamp();
-
-                $tr[] = $r;
-            }
+            default:
+                throw new \RuntimeException('Invalid Finder Type');
         }
 
-        if (count($tr) < 1) {
-            return new Comparator();
-        }
+        return $comparator;
+    }
 
-        $answer = $tr[0];
-
-        foreach ($tr as $result) {
-            switch ($type) {
-                case FinderType::CLOSTEST:
-                    if ($result->delta < $answer->delta) {
-                        $answer = $result;
-                    }
-                    break;
-
-                case FinderType::FURTHEST:
-                    if ($result->delta > $answer->delta) {
-                        $answer = $result;
-                    }
-                    break;
-            }
-        }
-
-        return $answer;
+    private static function calculateDelta(Person $firstPerson, Person $secondPerson): int
+    {
+        return $secondPerson->birthDate->getTimestamp() - $firstPerson->birthDate->getTimestamp();
     }
 }
